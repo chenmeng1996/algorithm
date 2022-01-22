c63ead465d1c5b3f4a127660dd7d5950042167ae
chenmeng <2786859763@qq.com>
2022-01-22 16:01:13 +0800

    二叉树非递归遍历

--- tree/遍历二叉树/非递归中序遍历.py
+++ tree/遍历二叉树/非递归中序遍历.py
@@ -1,7 +1,30 @@
 from typing import List
 from tree import TreeNode, build_tree1
 
-def traverse(root: TreeNode) -> List[TreeNode]:
+def _push_left_to_stack(stack: list, root: TreeNode):
+    while root is not None:
+        stack.append(root)
+        root = root.left
+
+def traverse(root: TreeNode):
+    stack = []
+    visited = None
+    res = []
+    _push_left_to_stack(stack, root)
+    while len(stack) != 0:
+        top = stack[-1]
+        # 左子树被访问过且右子树没被访问
+        if top.left is None or (top.left == visited and top.right != visited):
+            res.append(top)
+            visited = stack.pop()
+            _push_left_to_stack(stack, top.right)
+
+
+
+
+
+
+def traverse2(root: TreeNode) -> List[TreeNode]:
     res = []
     stack = []
     current = root
@@ -19,6 +42,6 @@ def traverse(root: TreeNode) -> List[TreeNode]:
 
 if __name__ == "__main__":
     root = build_tree1()
-    res = traverse(root)
+    res = traverse2(root)
     for node in res:
         print(node.value)
--- tree/遍历二叉树/非递归后序遍历.py
+++ tree/遍历二叉树/非递归后序遍历.py
@@ -2,7 +2,29 @@ from typing import List
 from tree import TreeNode, build_tree1
 from queue import Queue
 
-def traverse(root: TreeNode) -> List[TreeNode]:
+
+def _push_left_to_stack(stack: list, root: TreeNode):
+    while root is not None:
+        stack.append(root)
+        root = root.left
+
+def traverse(root: TreeNode):
+    stack = []
+    visited = None
+    res = []
+    _push_left_to_stack(stack, root)
+    while len(stack) != 0:
+        top = stack[-1]
+        # 左子树被访问过且右子树没被访问
+        if top.left is None or (top.left == visited and top.right != visited):
+            _push_left_to_stack(stack, top.right)
+        # 右子树访问过
+        if top.right is None or top.right == visited:
+            res.append(top)
+            visited = stack.pop()
+
+
+def traverse2(root: TreeNode) -> List[TreeNode]:
     '''队列记录下一个要处理的节点。
     栈记录处理过的节点'''
     q = Queue()
@@ -22,6 +44,6 @@ def traverse(root: TreeNode) -> List[TreeNode]:
     
 if __name__ == "__main__":
     root = build_tree1()
-    res = traverse(root)
+    res = traverse2(root)
     for node in res:
         print(node.value)

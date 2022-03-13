a6cae22a68fe86bfe9c0c1e43aabe08766008c28
陈濛 <chenmeng.yzy@bytedance.com>
2022-03-13 23:05:45 +0800

    动态规划

---
+++ dp/最长公共子序列.py
@@ -0,0 +1,25 @@
+
+from operator import le
+
+
+def longest_common_subsequence(s1: str, s2: str):
+    '''
+    dp[i][j]表示s1[0:i]和s2[0:j]的最长公共子序列长度
+    如果s1[i-1] = s2[j-1], 则dp[i][j] = dp[i-1][j-1] + 1
+    如果s1[i-1] != s2[j-1], 则dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+    dp[0][j] = 0
+    dp[i][0] = 0
+    '''
+    dp = [[0 for _ in range(len(s2)+1)] for _ in range(len(s1)+1)]
+    for i in range(1, len(s1)+1):
+        for j in range(1, len(s2)+1):
+            if s1[i-1] == s2[j-1]:
+                dp[i][j] = dp[i-1][j-1] + 1
+            else:
+                dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+    return dp[len(s1)][len(s2)]
+
+
+if __name__ == "__main__":
+    res = longest_common_subsequence("abcde", "ace")
+    print(res)
---
+++ dp/编辑距离.py
@@ -0,0 +1,2 @@
+def minDistance(s1: str, s2: str) -> int:
+    pass

51a90d1f7d1a8fe5e76d8fa8cf065dfe430aa2ef
陈濛 <chenmeng.yzy@bytedance.com>
2022-05-19 14:46:44 +0800

    刷

--- 动态规划/单词拆分.py
+++ 动态规划/单词拆分.py
@@ -4,6 +4,11 @@ from typing import List
 
 """
 https://leetcode-cn.com/problems/word-break/
+
+给你一个字符串 s 和一个字符串列表 wordDict 作为字典。
+请你判断是否可以利用字典中出现的单词拼接出 s 。
+
+注意：不要求字典中出现的单词全部都使用，并且字典中的单词可以重复使用
 """
 def wordBreak(s: str, wordDict: List[str]) -> bool:
     """
@@ -11,7 +16,7 @@ def wordBreak(s: str, wordDict: List[str]) -> bool:
     背包问题-True/False问题
     dp[i] = dp[i] or dp[i-num]
 
-    dp[i]表示以i结尾的字符串，是否可以由字典拼接而成。
+    dp[i]表示以i结尾的字符串, 是否可以由字典拼接而成。
     dp[i] = (s[0:i+1] in wordDict) or (dp[0] is True and s[1:i+1] in wordDict) or .... or (dp[i-1] is True and s[i:i+1] in wordDict)
     """
     wordSet = set()
